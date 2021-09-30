cede587ee100f281e3256751cd1b88b7a7a20466
tjbw666 <78714838+tjbw666@users.noreply.github.com>
2021-09-30 09:16:31 +0800

    Add files via upload

---
+++ test01/源.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+bool IsPalindereme(char* str, size_t size)
+{
+	int begin = 0;
+	int end = size - 1;
+	while (begin < size)
+	{
+		if (str[begin] != str[end])
+			return false;
+		++begin;
+		--end;
+	}
+	return true;
+}
+void Funtest()
+{
+	cout << IsPalindereme("1234321", 7) << endl;
+	cout << IsPalindereme("", 0) << endl;//特殊情况
+	cout << IsPalindereme("1221", 4) << endl;
+	cout << IsPalindereme("1", 1) << endl;
+	cout << IsPalindereme("1231", 4) << endl;
+}
+int main()
+{
+	Funtest();
+	system("pause");
+	return 0;
+}
